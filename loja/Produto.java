8cce31191e28683bcf82ccf7d8e16da9ac6874d0
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-03-24 03:49:06 -0300

    simulando ambiente de loja com oop

---
+++ loja/Produto.java
@@ -0,0 +1,49 @@
+package loja;
+
+public class Produto {
+    String nome;
+    double preco;
+    int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerEstoque(int quantidade) {
+        if(this.quantidade >= quantidade) {
+            this.quantidade -= quantidade;
+        } else {
+            System.out.println("Quantidade insuficiente em estoque.");
+        }
+    }
+
+    public int consultarEstoque() {
+        return this.quantidade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    @Override // sobrescreve o método toString da classe Object
+    public String toString() {
+        return nome + " - Preço: U$" + preco + " - Quantidade: " + quantidade;
+    }
+
+
+
+}
---
+++ loja/Teste.java
@@ -0,0 +1,41 @@
+package loja;
+
+public class Teste {
+    public static void main(String[] args) {
+        Estoque estoque = new Estoque();
+
+        Produto produto1 = new Produto("Camiseta", 50.0, 3);
+        Produto produto2 = new Produto("Calça", 100.0, 2);
+
+        estoque.exibeEstoque();
+        System.out.println("\n");
+
+        estoque.adicionarProdutos(produto1);
+        estoque.adicionarProdutos(produto2);
+        estoque.exibeEstoque();
+        System.out.println("Produtos add\n");
+
+        produto1.removerEstoque(2);
+        produto2.adicionarEstoque(1);
+        estoque.exibeEstoque();
+        System.out.println("Produtos removidos, mas também adicionados\n");
+
+        Produto produtoConsultado = estoque.consultarProduto("Camisetas");
+        if(produtoConsultado != null) {
+            System.out.println("Quantidade de camisetas em estoque: " + produtoConsultado.getQuantidade());
+        } else {
+            System.out.println("Produto não encontrado.");
+        }
+
+        System.out.println("\n");
+
+        estoque.exibeEstoque();
+        System.out.println("\n");
+
+        estoque.removerProduto("Camiseta");
+        estoque.exibeEstoque();
+
+
+
+    }
+}
